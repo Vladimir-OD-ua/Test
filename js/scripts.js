e2d598f552e798c683dd62fbbc43a16cbf7c146f
Vladimir-OD-ua <62292624+Vladimir-OD-ua@users.noreply.github.com>
2020-12-11 23:36:04 +0200

    Add all project files

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+const compareList = [];
+const favouritesList = [];
+
+const scripts = () => {
+    const selectHeader = document.querySelectorAll('.scripts-header');
+    let selectItem = document.querySelectorAll('.scripts-item');
+
+    selectHeader.forEach(item=> {
+        item.addEventListener('click', selectToggle)
+    });
+
+    selectItem.forEach(item=> {
+        item.addEventListener('click', selectChoose)
+    });
+    
+    function selectToggle() {
+        this.parentElement.classList.toggle('is-active');
+    }
+    
+    function selectChoose() {
+        let text = this.innerText,
+            select = this.closest('.scripts'),
+            selectCurrent = select.querySelector('.scripts-current');
+        selectCurrent.innerText = text;
+        selectCurrent.classList.add('dirty');
+        select.classList.remove('is-active');
+    }
+};
+
+scripts();
+
+const reset = () => {
+    const selects = document.querySelectorAll('.scripts');
+
+    selects.forEach(select => {
+        if (!select.classList.contains('category-scripts'))
+            select.querySelector('.scripts-current').innerText = '';
+    });
+
+    const inputs = document.querySelectorAll('.filter-price-input');
+
+    inputs.forEach(input => {
+        input.value = ''
+    })
+
+};
+
+const onCompareButtonClick = id => {
+    const buttonText = document.querySelector(`#${id} span`);
+    const compareItemsCountText = document.getElementById('comparison');
+
+    if (compareList.includes(id)) {
+        compareList.splice(id, 1);
+        buttonText.innerText = 'Сравнить товар';
+    } else {
+        compareList.push(id);
+        buttonText.innerText = 'В сравнении';
+    }
+    if (compareList.length) compareItemsCountText.innerText = compareList.length.toString();
+    else compareItemsCountText.innerText = ''
+};
+
+const onFavouritesButtonClick = id => {
+    const buttonText = document.querySelector(`#${id} span`);
+    const favoritesItemsCountText = document.getElementById('favourites');
+
+    if (favouritesList.includes(id)) {
+        favouritesList.splice(id, 1);
+        buttonText.innerText = 'В избраное';
+    } else {
+        favouritesList.push(id);
+        buttonText.innerText = 'В избраном';
+    }
+    if (favouritesList.length) favoritesItemsCountText.innerText = favouritesList.length.toString();
+    else favoritesItemsCountText.innerText = ''
+};
+
+
