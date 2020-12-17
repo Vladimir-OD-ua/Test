52c5c9f61940384986b780cc3116c34b20bd493a
Vladimir-OD-ua <62292624+Vladimir-OD-ua@users.noreply.github.com>
2020-12-17 02:58:40 +0200

    Addition of website elements

--- js/scripts.js
+++ js/scripts.js
@@ -76,3 +76,100 @@ const onFavouritesButtonClick = id => {
 };
 
 
+document.querySelector('.mobile-menu-btn').addEventListener('click', () => {
+    document.querySelector('.mobile-menu').classList.toggle('mobile-menu-active');
+});
+
+const callbackForm = document.querySelector('.callback-btn');
+const callbackModal = document.querySelector('#callback-modal');
+const registrationBtn = document.querySelector('.btn-enter');
+const registrationModal = document.querySelector('#registration-modal');
+const loadingFormBtn = document.querySelector('#loading-form-btn');
+const callbackFormBtn = document.querySelector('.callback-modal-btn');
+const userEmail = document.querySelector('#loading-form-input-email');
+const userPhone = document.querySelector('#callback-form-input-phone');
+const userPassword = document.querySelector('#loading-form-input-password');
+
+callbackForm.addEventListener('click', function () {
+
+    callbackModal.classList.add('modal-active');
+    closeOnClickOutSide(callbackModal);
+    document.querySelector(`#${callbackModal.id} .modal-close-btn`).addEventListener('click', function () {
+
+        callbackModal.classList.remove('modal-active');
+    });
+
+});
+
+callbackFormBtn.addEventListener('click', function () {
+    event.preventDefault();
+    let hasError = false;
+
+    if (!userPhone.value.trim() || !isPhoneValid(userPhone.value)) {
+        userPhone.classList.add('form-input-error');
+        hasError = true;
+    } else {
+        userPhone.classList.remove('form-input-error');
+        callbackModal.classList.remove('modal-active');
+    }
+    if (hasError) {
+        return;
+    }
+});
+
+registrationBtn.addEventListener('click', function () {
+
+    registrationModal.classList.add('modal-active');
+    closeOnClickOutSide(registrationModal);
+    document.querySelector(`#${registrationModal.id} .modal-close-btn`).addEventListener('click', function () {
+
+        registrationModal.classList.remove('modal-active');
+    });
+});
+
+loadingFormBtn.addEventListener('click', function () {
+    event.preventDefault();
+    let hasError = false;
+
+    if (!userEmail.value.trim() || !isEmailValid(userEmail.value)) {
+        userEmail.classList.add('form-input-error');
+        hasError = true;
+    } else {
+        userEmail.classList.remove('form-input-error');
+        registrationModal.classList.remove('modal-active');
+    }
+
+    if (!userPassword.value.trim() || ('')) {
+        userPassword.classList.add('form-input-error');
+        hasError = true;
+    } else {
+        userPassword.classList.remove('form-input-error');
+        registrationModal.classList.remove('modal-active');
+    }
+
+    if (hasError) {
+        return;
+    }
+});
+
+const closeOnClickOutSide = modal => {
+    document.body.addEventListener('click', function (event)
+    {
+        if (event.target.className === 'modal-container') {
+            modal.classList.remove('modal-active');
+        }
+    });
+};
+
+function isPhoneValid(phone = '') {
+    const regexp = /(\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4})/;
+
+    return phone.match(regexp);
+}
+
+function isEmailValid(email) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email.toLowerCase());
+}
+
+
